4bf9607de5560d6ca6c5d6f699467946d701f0ac
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-09-26 17:10:54 -0400

    Fix layouts

---
+++ src/routes/(special)/auth/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../../../app.css";
+</script>
+
+<div id="page-content" class="w-screen h-screen">
+  <slot />
+</div>
--- src/routes/publish/+page.svelte
+++ src/routes/publish/+page.svelte
@@ -1,18 +1,20 @@
-<div class="flex flex-col space-y-4 max-w-sm">
-  <h1 class="text-3xl font-bold">Publish world</h1>
-  <form id="publish-world" method="POST" action="?/publish" class="flex flex-col flex-1 space-y-2">
-    <div class="form-control">
-      <div class="label">
-        <span class="label-text">Place Id</span>
+<div class="p-12">
+  <div class="flex flex-col space-y-4 max-w-sm">
+    <h1 class="text-3xl font-bold">Publish world</h1>
+    <form id="publish-world" method="POST" action="?/publish" class="flex flex-col flex-1 space-y-2">
+      <div class="form-control">
+        <div class="label">
+          <span class="label-text">Place Id</span>
+        </div>
+        <input name="placeId" type="number" placeholder="123456789" required class="input">
       </div>
-      <input name="placeId" type="number" placeholder="123456789" required class="input">
-    </div>
-    <div class="form-control">
-      <label for="tosAccepted" class="label">
-        <span class="label-text">I agree to both the <a class="link" target="_blank" href="https://en.help.roblox.com/hc/en-us/articles/115004647846-Roblox-Terms-of-Use">Roblox</a> and <a class="link" target="_blank" href="/terms">RoRooms</a> Terms of Service</span>
-        <input name="tosAccepted" type="checkbox" class="checkbox checkbox-primary" id="tosAccepted" required>
-      </label>
-    </div>
-    <button class="btn btn-primary">Publish!</button>
-  </form>
+      <div class="form-control">
+        <label for="tosAccepted" class="label">
+          <span class="label-text">I agree to both the <a class="link" target="_blank" href="https://en.help.roblox.com/hc/en-us/articles/115004647846-Roblox-Terms-of-Use">Roblox</a> and <a class="link" target="_blank" href="/terms">RoRooms</a> Terms of Service</span>
+          <input name="tosAccepted" type="checkbox" class="checkbox checkbox-primary" id="tosAccepted" required>
+        </label>
+      </div>
+      <button class="btn btn-primary">Publish!</button>
+    </form>
+  </div>
 </div>
