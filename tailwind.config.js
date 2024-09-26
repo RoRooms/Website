e1c25b21f9e0de660e387fce358020ac97a05126
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-09-25 21:25:25 -0400

    Theme DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
+import { dark } from 'daisyui/src/theming/themes';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -9,5 +10,17 @@ export default {
   plugins: [
     daisyui
   ],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...dark,
+          primary: "#8566FF",
+          primaryGradient: "linear-gradient(to bottom, #5033ff 0%,#7c5bff 100%)",
+          background: "black"
+        }
+      },
+    ]
+  }
 }
 
