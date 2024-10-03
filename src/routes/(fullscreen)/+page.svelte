9fa3bdb2767909c0fd9a994afb5381017f6431eb
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-10-03 18:56:01 -0400

    Add background effects

--- src/routes/(fullscreen)/+page.svelte
+++ src/routes/(fullscreen)/+page.svelte
@@ -7,17 +7,26 @@
 </script>
 
 <div class="w-full flex flex-col">
-  <div id="intro" class="w-full flex flex-col items-center space-y-16 p-24 pt-40 pb-40 bg-cover bg-[url($lib/media/Banner.png)]">
+  <div id="intro" class="w-full flex flex-col items-center space-y-16 p-24 pt-40 pb-40 bg-cover bg-[url($lib/media/Banner.png)] drop-shadow-[0_300px_100px_#8566FF20]">
     <h1 class="text-5xl font-black text-white drop-shadow-md text-center">Elevate your worlds.</h1>
     <span class="flex flex-row space-x-3">
       <a href="/install" class="btn btn-secondary">Install</a>
       <a href="/discord" class="btn btn-secondary btn-outline">Discuss</a>
     </span>
   </div>
-  <div id="points" class="w-full p-4 pt-16 pb-16 space-y-6 flex flex-col items-center">
-    <Section heading="Roleplay toolkit 🙋" description="With profiles, items, emotes and more, your world has everything it needs to engage players." type="video" src={InterfaceDemo} />
-    <Section reversed heading="Worlds network 🌐" description="By participating in the universal worlds network, you can reach more players than possible alone." src={NetworkPattern} />
-    <Section heading="Players-first 🙌" description="A seamless social experience, hopping between worlds with friends, and enjoying more time in-game." src={PlayersFirst} />
-    <Section reversed heading="Free and open 🕊️" description="It's all customizable and open source, so you can change whatever you like. They're your worlds, after all." src={GitHub} />
+  <div id="points" class="w-full px -4 flex flex-col items-center">
+    <span class="space-y-6 flex-col bg-black lg:p-20 py-24">
+      <Section heading="Roleplay toolkit 🙋" description="With profiles, items, emotes and more, your world has everything it needs to engage players." type="video" src={InterfaceDemo} />
+      <Section reversed heading="Worlds network 🌐" description="By participating in the universal worlds network, you can reach more players than possible alone." src={NetworkPattern} />
+      <Section heading="Players-first 🙌" description="A seamless social experience, hopping between worlds with friends, and enjoying more time in-game." src={PlayersFirst} />
+      <Section reversed heading="Free and open 🕊️" description="It's all customizable and open source, so you can change whatever you like. They're your worlds, after all." src={GitHub} />
+    </span>
   </div>
 </div>
+
+<style>
+  #points {
+    background-image: url("data:image/svg+xml,%3Csvg width='36' height='36' viewBox='0 0 36 36' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M36 0H0v36h36V0zM15.126 2H2v13.126c.367.094.714.24 1.032.428L15.554 3.032c-.188-.318-.334-.665-.428-1.032zM18 4.874V18H4.874c-.094-.367-.24-.714-.428-1.032L16.968 4.446c.318.188.665.334 1.032.428zM22.874 2h11.712L20 16.586V4.874c1.406-.362 2.512-1.468 2.874-2.874zm10.252 18H20v13.126c.367.094.714.24 1.032.428l12.522-12.522c-.188-.318-.334-.665-.428-1.032zM36 22.874V36H22.874c-.094-.367-.24-.714-.428-1.032l12.522-12.522c.318.188.665.334 1.032.428zm0-7.748V3.414L21.414 18h11.712c.362-1.406 1.468-2.512 2.874-2.874zm-18 18V21.414L3.414 36h11.712c.362-1.406 1.468-2.512 2.874-2.874zM4.874 20h11.712L2 34.586V22.874c1.406-.362 2.512-1.468 2.874-2.874z' fill='%239C92AC' fill-opacity='0.07' fill-rule='evenodd'/%3E%3C/svg%3E");
+    background-size: 70px 70px;
+  }
+</style>
