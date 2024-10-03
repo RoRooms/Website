5299998993ad49b92de125eb593cc28d8c0c5efd
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-10-03 17:45:31 -0400

    Fix section reversal

--- src/lib/components/section.svelte
+++ src/lib/components/section.svelte
@@ -4,17 +4,15 @@
   export let type: "img" | "video" = "img";
   export let src;
   export let reversed = false;
-
-  $: order = reversed ? 2 : 1;
 </script>
 
 <article class="md:flex md:space-y-0 rounded-xl border-2 overflow-hidden border-white/20 hover:bg-white/10 transition-all duration-300">
-  <span class="flex-col space-y-0 md:space-y-6 p-8 max-w-lg order-{order}">
+  <span style="order: {reversed ? 2 : 1};" class="flex-col space-y-0 md:space-y-6 p-8 max-w-lg">
     <h1 class="text-4xl font-black text-white">{heading}</h1>
 
     <p>{description}</p>
   </span>
-  <span class="md:shrink-0 w-full h-64 md:w-80 order-1 overflow-hidden relative flex items-center place-content-center">
+  <span style="order: {reversed ? 1 : 2};" class="md:shrink-0 w-full h-64 md:w-80 overflow-hidden relative flex items-center place-content-center">
     {#if type == "img"}
       <span class="w-full h-full bg-center bg-cover" style="background-image: url({src});" />
     {:else if type == "video"}
