185fba3c59805cb7a8c5c15e52e66c7bd6e4d5ba
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-10-07 14:24:32 -0400

    Adapt to tutorial

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,7 @@
-// import type { LayoutServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 
-// export const load: LayoutServerLoad = async (event) => {
-// 	const session = await event.locals.auth();
-
-// 	return {
-// 		session
-// 	};
-// };
+export const load: LayoutServerLoad = async (event) => {
+	return {
+		session: await event.locals.auth()
+	};
+};
