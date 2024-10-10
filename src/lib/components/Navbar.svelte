42a0b2f865e7eb43b682a4e9c8079a7b0efbc5b3
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-10-10 15:44:30 -0400

    Fix navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Logo from "$lib/components/logo.svelte";
+  import Headshot from "$lib/components/Headshot.svelte";
+</script>
+
+<div class="navbar pl-8 pr-8 bg-zinc-900 overflow-hidden fixed top-0 z-10 drop-shadow-md">
+  <div class="navbar-start">
+    <a href="/">
+      <Logo classes="h-9 hover:drop-shadow-[0_4px_4px_rgba(255,255,255,0.3)] duration-300" />
+    </a>
+  </div>
+
+  <div class="navbar-center lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/install" class="font-bold">Install</a></li>
+      <li><a href="/docs" class="font-bold">Docs</a></li>
+      <li><a href="/publish" class="font-bold">Publish</a></li>
+    </ul>
+  </div>
+
+  <span class="navbar-end">
+    <Headshot />
+  </span>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
   import "../app.css";
-  import Logo from "$lib/components/logo.svelte";
-  import Headshot from "$lib/components/Headshot.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
 </script>
 
-<div class="w-full h-screen flex flex-col">
-  <div class="navbar pl-8 pr-8 bg-zinc-900 overflow-hidden sticky top-0 z-10 drop-shadow-md">
-    <div class="navbar-start">
-      <a href="/">
-        <Logo classes="h-9 hover:drop-shadow-[0_4px_4px_rgba(255,255,255,0.3)] duration-300" />
-      </a>
-    </div>
-
-    <div class="navbar-center lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a href="/install" class="font-bold">Install</a></li>
-        <li><a href="/docs" class="font-bold">Docs</a></li>
-        <li><a href="/publish" class="font-bold">Publish</a></li>
-      </ul>
-    </div>
-
-    <span class="navbar-end">
-      <Headshot />
-    </span>
-  </div>
+<div class="w-full h-full flex flex-col mt-16 overflow-hidden">
+  <Navbar />
 
   <slot />
 </div>
